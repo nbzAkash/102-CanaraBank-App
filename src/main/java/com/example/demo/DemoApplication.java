414d78ff538448a0163f6e81d0d612433b56bc5b
nbzAkash <akashgupta.nbz@gmail.com>
2022-07-30 17:29:17 +0530

    csl-123 fixed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+              
+			  int i=10;
+			  
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
